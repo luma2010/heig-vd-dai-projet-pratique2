b524e84bddaaf83546f4d0f57242ed5d348314a4
Nekoth <alexandre.deletraz@hotmail.com>
2024-11-15 19:16:10 +0100

    Ajout de la class Delete

--- src/main/java/ch/heigvd/dai/Delete.java
+++ src/main/java/ch/heigvd/dai/Delete.java
@@ -7,44 +7,47 @@ import java.util.List;
 import java.util.Objects;
 
 public class Delete {
-    try (
-    InputStream in = new FileInputStream(parent.getFileName());
-    Reader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
-    BufferedReader br = new BufferedReader(reader)) {
 
-        boolean varExists = false;
-        List<String> lines = new ArrayList<>();
-        String line;
+    public static void deleteVariableToFile(String fileName, String varName) {
+        try (
+                InputStream in = new FileInputStream(fileName);
+                Reader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
+                BufferedReader br = new BufferedReader(reader)) {
 
-        // Lecture du fichier et vérification de l'existence de la variable
-        while ((line = br.readLine()) != null) {
-            String[] part = line.split("\\=");
-            if (Objects.equals(part[0], varName)) {
-                varExists = true;
-            } else {
-                lines.add(line + "\n");
-            }
-        }
+            boolean varExists = false;
+            List<String> lines = new ArrayList<>();
+            String line;
 
-        // Si la variable existe, on réécrit le fichier sans elle.
-        if (varExists) {
-            try (OutputStream os = new FileOutputStream(parent.getFileName());
-                 Writer writer = new OutputStreamWriter(os, StandardCharsets.UTF_8);
-                 BufferedWriter bw = new BufferedWriter(writer)) {
-                for (String s : lines) {
-                    bw.write(s);
+            // Lecture du fichier et vérification de l'existence de la variable
+            while ((line = br.readLine()) != null) {
+                String[] part = line.split("=");
+                if (Objects.equals(part[0], varName)) {
+                    varExists = true;
+                } else {
+                    lines.add(line + "\n");
                 }
-                bw.flush();
-            } catch (IOException e) {
-                System.out.println("Exception: " + e);
             }
-            System.out.println("Variable " + varName + " has been deleted.");
-        } else {
-            System.out.println("Variable " + varName + " not found, nothing changed.");
-        }
 
-    } catch (IOException e) {
-        System.out.println("Exception: " + e);
+            // Si la variable existe, on réécrit le fichier sans elle.
+            if (varExists) {
+                try (OutputStream os = new FileOutputStream(fileName);
+                     Writer writer = new OutputStreamWriter(os, StandardCharsets.UTF_8);
+                     BufferedWriter bw = new BufferedWriter(writer)) {
+                    for (String s : lines) {
+                        bw.write(s);
+                    }
+                    bw.flush();
+                } catch (IOException e) {
+                    System.out.println("Exception: " + e);
+                }
+                System.out.println("Variable " + varName + " has been deleted.");
+            } else {
+                System.out.println("Variable " + varName + " not found, nothing changed.");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Exception: " + e);
+        }
     }
 }
-}
+
